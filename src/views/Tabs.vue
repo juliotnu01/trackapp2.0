d23d1839170fe4400251472fb539559200a715d3
juliot nuñez <julio@nexia.io>
2021-12-09 13:36:48 -0500

    commit temporal

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -1,62 +1,58 @@
 <template>
-  <ion-page>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon name="person-outline"></ion-icon>
-          <ion-label>Mi Perfil </ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon name="map-outline"></ion-icon>
-          <ion-label>Mapa</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon name="notifications-outline"></ion-icon>
-          <ion-label>Notificaciones</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab4" href="/tabs/tab4">
-          <ion-icon name="options-outline"></ion-icon>
-          <ion-label><small> Configuración </small></ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  </ion-page>
+    <ion-page>
+        <ion-tabs>
+            <ion-router-outlet></ion-router-outlet>
+            <ion-tab-bar slot="bottom">
+                <ion-tab-button tab="tab1" href="/tabs/tab1">
+                    <ion-icon name="person-outline"></ion-icon>
+                    <ion-label>Mi Perfil </ion-label>
+                </ion-tab-button>
+                <ion-tab-button tab="tab2" href="/tabs/tab2">
+                    <ion-icon name="map-outline"></ion-icon>
+                    <ion-label>Mapa</ion-label>
+                </ion-tab-button>
+                <ion-tab-button tab="tab3" href="/tabs/tab3">
+                    <ion-icon name="notifications-outline"></ion-icon>
+                    <ion-label>Notificaciones</ion-label>
+                </ion-tab-button>
+                <ion-tab-button tab="tab4" href="/tabs/tab4">
+                    <ion-icon name="options-outline"></ion-icon>
+                    <ion-label><small> Configuración </small></ion-label>
+                </ion-tab-button>
+            </ion-tab-bar>
+        </ion-tabs>
+    </ion-page>
 </template>
-
 <script>
 /* eslint-disable */
 import {
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  IonLabel,
-  IonIcon,
-  IonPage,
-  IonRouterOutlet,
+    IonTabBar,
+    IonTabButton,
+    IonTabs,
+    IonLabel,
+    IonIcon,
+    IonPage,
+    IonRouterOutlet,
 } from "@ionic/vue";
 // import { ellipse, square, triangle } from 'ionicons/icons';
 
 export default {
-  name: "Tabs",
-  components: {
-    IonLabel,
-    IonTabs,
-    IonTabBar,
-    IonTabButton,
-    IonIcon,
-    IonPage,
-    IonRouterOutlet,
-  },
-  setup() {
-    return {
-      // ellipse,
-      // square,
-      // triangle,
-    };
-  },
+    name: "Tabs",
+    components: {
+        IonLabel,
+        IonTabs,
+        IonTabBar,
+        IonTabButton,
+        IonIcon,
+        IonPage,
+        IonRouterOutlet,
+    },
+    setup() {
+        return {
+            // ellipse,
+            // square,
+            // triangle,
+        };
+    },
 };
 </script>
---
+++ src/views/viewUnit.vue
@@ -0,0 +1,60 @@
+<template>
+    <ion-app>
+        <ion-content>
+           <!--  <ion-grid>
+                <ion-row>
+                    <ion-col size="12" size-sm></ion-col>
+                    <ion-col> </ion-col>
+                </ion-row>
+            </ion-grid> -->
+            <h1>view unit details</h1>
+        </ion-content>
+    </ion-app>
+</template>
+<script>
+import { 
+  IonApp, 
+  // IonRow, 
+  // IonCol, 
+  // IonGrid, 
+  IonContent } from "@ionic/vue";
+
+import { 
+  defineComponent, 
+  // ref, 
+  onMounted } from "vue";
+// import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default defineComponent({
+    name: "App",
+    components: {
+        IonApp,
+        // IonRow,
+        // IonCol,
+        // IonGrid,
+        IonContent,
+    },
+
+    setup() {
+        // let _mymap = ref(null);
+        // const initMap = () => {
+        //     _mymap.value = L.map("mapid").setView([10.6764195, -71.6881817], 2);
+        //     L.tileLayer(
+        //         "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibnVuZXpqdWxpb3QiLCJhIjoiY2t1NG9pNTk3MW8ydDJ4cWdpNnV4ZnZ6aSJ9.b9aMsL-D9kJ09lm4pnzzEg", {
+        //             attribution: "",
+        //             maxZoom: 18,
+        //             id: "mapbox/streets-v11",
+        //             tileSize: 512,
+        //             zoomOffset: -1,
+        //             accessToken: "pk.eyJ1IjoibnVuZXpqdWxpb3QiLCJhIjoiY2t1NG9pNTk3MW8ydDJ4cWdpNnV4ZnZ6aSJ9.b9aMsL-D9kJ09lm4pnzzEg",
+        //         }
+        //     ).addTo(_mymap.value);
+        // };
+        onMounted(() => {
+            // initMap();
+        });
+        return {};
+    },
+});
+</script>
